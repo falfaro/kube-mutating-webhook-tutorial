4f22c596d0365f81eaa42b501ca5e3a5df80fcdd
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2019-03-22 13:32:52 +0100

    Clean up code.

--- webhook.go
+++ webhook.go
@@ -30,20 +30,20 @@ var (
 	defaulter = runtime.ObjectDefaulter(runtimeScheme)
 )
 
-type WebhookServer struct {
-	sidecarConfig *Config
+type webhookServer struct {
+	sidecarConfig *config
 	server        *http.Server
 }
 
 // Webhook Server parameters
-type WhSvrParameters struct {
+type whSvrParameters struct {
 	port           int    // webhook server port
 	certFile       string // path to the x509 certificate for https
 	keyFile        string // path to the x509 private key matching `CertFile`
 	sidecarCfgFile string // path to sidecar injector configuration file
 }
 
-type Config struct {
+type config struct {
 	Containers []corev1.Container `yaml:"containers"`
 	Volumes    []corev1.Volume    `yaml:"volumes"`
 }
@@ -62,24 +62,14 @@ func init() {
 	_ = v1.AddToScheme(runtimeScheme)
 }
 
-// (https://github.com/kubernetes/kubernetes/issues/57982)
-func applyDefaultsWorkaround(containers []corev1.Container, volumes []corev1.Volume) {
-	defaulter.Default(&corev1.Pod{
-		Spec: corev1.PodSpec{
-			Containers: containers,
-			Volumes:    volumes,
-		},
-	})
-}
-
-func loadConfig(configFile string) (*Config, error) {
+func loadConfig(configFile string) (*config, error) {
 	data, err := ioutil.ReadFile(configFile)
 	if err != nil {
 		return nil, err
 	}
 	glog.Infof("New configuration: sha256sum %x", sha256.Sum256(data))
 
-	var cfg Config
+	var cfg config
 	if err := yaml.Unmarshal(data, &cfg); err != nil {
 		return nil, err
 	}
@@ -87,171 +77,88 @@ func loadConfig(configFile string) (*Config, error) {
 	return &cfg, nil
 }
 
-func addContainer(target, added []corev1.Container, basePath string) (patch []patchOperation) {
-	first := len(target) == 0
-	var value interface{}
-	for _, add := range added {
-		value = add
-		path := basePath
-		if first {
-			first = false
-			value = []corev1.Container{add}
-		} else {
-			path = path + "/-"
-		}
-		patch = append(patch, patchOperation{
-			Op:    "add",
-			Path:  path,
-			Value: value,
-		})
-	}
-	return patch
-}
+// main mutation process
+func (whsvr *webhookServer) mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
+	req := ar.Request
 
-func addVolume(target, added []corev1.Volume, basePath string) (patch []patchOperation) {
-	first := len(target) == 0
-	var value interface{}
-	for _, add := range added {
-		value = add
-		path := basePath
-		if first {
-			first = false
-			value = []corev1.Volume{add}
-		} else {
-			path = path + "/-"
-		}
-		patch = append(patch, patchOperation{
-			Op:    "add",
-			Path:  path,
-			Value: value,
-		})
-	}
-	return patch
-}
-
-func updateAnnotation(target map[string]string, added map[string]string) (patch []patchOperation) {
-	for key, value := range added {
-		if target == nil || target[key] == "" {
-			target = map[string]string{}
-			patch = append(patch, patchOperation{
-				Op:   "add",
-				Path: "/metadata/annotations",
-				Value: map[string]string{
-					key: value,
-				},
-			})
-		} else {
-			patch = append(patch, patchOperation{
-				Op:    "replace",
-				Path:  "/metadata/annotations/" + key,
-				Value: value,
-			})
+	var ingress extensionsv1beta1.Ingress
+	if err := json.Unmarshal(req.Object.Raw, &ingress); err != nil {
+		// Error unmarshalling Ingress
+		return &v1beta1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
 		}
 	}
-	return patch
-}
 
-// create mutation patch for resoures
-func createPatch(pod *corev1.Pod, sidecarConfig *Config, annotations map[string]string) ([]byte, error) {
 	var patch []patchOperation
 
-	patch = append(patch, addContainer(pod.Spec.Containers, sidecarConfig.Containers, "/spec/containers")...)
-	patch = append(patch, addVolume(pod.Spec.Volumes, sidecarConfig.Volumes, "/spec/volumes")...)
-	patch = append(patch, updateAnnotation(pod.Annotations, annotations)...)
-
-	return json.Marshal(patch)
-}
-
-// main mutation process
-func (whsvr *WebhookServer) mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
-	req := ar.Request
-	if req.Kind.Version == "v1beta1" && req.Kind.Kind == "Ingress" {
-		var ingress extensionsv1beta1.Ingress
-		if err := json.Unmarshal(req.Object.Raw, &ingress); err != nil {
-			// Error unmarshalling Ingress
-			return &v1beta1.AdmissionResponse{
-				Result: &metav1.Status{
-					Message: err.Error(),
-				},
-			}
-		}
-
-		glog.Infof("Parsed Ingress: %s", ingress.String())
-		glog.Infof("Length of TLS: %v", len(ingress.Spec.TLS))
-
-		var patch []patchOperation
-
-		for tlsIndex, tls := range ingress.Spec.TLS {
-			glog.Infof("Length of %s: %v", tls.SecretName, len(tls.Hosts))
-			if len(tls.Hosts) == 0 {
-				// Need to generate a patch to add a single FQDN Host
-				// derived from the Ingress name and BKPR's DNS domain
-				glog.Infof("No Hosts for %s", tls.SecretName)
-				patch = append(patch, patchOperation{
-					Op:    "replace",
-					Path:  fmt.Sprintf("/spec/tls/%d/hosts", tlsIndex),
-					Value: []string{"cafe.eks.felipe-alfaro.com"},
-				})
-			} else {
-				for hostIndex, host := range tls.Hosts {
-					if len(host) == 0 {
-						// Empty Host: need to generate to replace its value with
-						// one derived from the Ingress name and BKPR's domain
-						glog.Info("Parsed No Host")
+	for tlsIndex, tls := range ingress.Spec.TLS {
+		glog.Infof("Length of %s: %v", tls.SecretName, len(tls.Hosts))
+		if len(tls.Hosts) == 0 {
+			// Need to generate a patch to add a single FQDN Host
+			// derived from the Ingress name and BKPR's DNS domain
+			glog.Infof("No Hosts for %s", tls.SecretName)
+			patch = append(patch, patchOperation{
+				Op:    "replace",
+				Path:  fmt.Sprintf("/spec/tls/%d/hosts", tlsIndex),
+				Value: []string{"cafe.eks.felipe-alfaro.com"},
+			})
+		} else {
+			for hostIndex, host := range tls.Hosts {
+				if len(host) == 0 {
+					// Empty Host: need to generate to replace its value with
+					// one derived from the Ingress name and BKPR's domain
+					glog.Info("Parsed No Host")
+					patch = append(patch, patchOperation{
+						Op:    "replace",
+						Path:  fmt.Sprintf("/spec/tls/%d/hosts/%d", tlsIndex, hostIndex),
+						Value: "cafe.eks.felipe-alfaro.com",
+					})
+				} else {
+					// Check whether Host is a FQDN
+					v := govalidator.New()
+					if err := v.Var(host, "fqdn"); err == nil {
+						glog.Infof("Parsed FQDN Host: %s", host)
+					} else {
+						// Non-FQDN: need to qualify the Host with BKPR's
+						// domain
+						newHost := host
+						if !strings.HasSuffix(host, ".") {
+							newHost += "."
+						}
+						newHost += "eks.felipe-alfaro.com"
 						patch = append(patch, patchOperation{
 							Op:    "replace",
 							Path:  fmt.Sprintf("/spec/tls/%d/hosts/%d", tlsIndex, hostIndex),
-							Value: "cafe.eks.felipe-alfaro.com",
+							Value: newHost,
 						})
-					} else {
-						// Check whether Host is a FQDN
-						v := govalidator.New()
-						if err := v.Var(host, "fqdn"); err == nil {
-							glog.Infof("Parsed FQDN Host: %s", host)
-						} else {
-							// Non-FQDN: need to qualify the Host with BKPR's
-							// domain
-							newHost := host
-							if !strings.HasSuffix(host, ".") {
-								newHost += "."
-							}
-							newHost += "eks.felipe-alfaro.com"
-							patch = append(patch, patchOperation{
-								Op:    "replace",
-								Path:  fmt.Sprintf("/spec/tls/%d/hosts/%d", tlsIndex, hostIndex),
-								Value: newHost,
-							})
-							glog.Infof("Parsed non-FQDN: %s into: %s", host, newHost)
-						}
+						glog.Infof("Parsed non-FQDN: %s into: %s", host, newHost)
 					}
 				}
 			}
 		}
+	}
 
-		patchBytes, err := json.Marshal(patch)
-		if err != nil {
-			glog.Error("Error marshalling patch!")
-			return &v1beta1.AdmissionResponse{
-				Result: &metav1.Status{
-					Message: err.Error(),
-				},
-			}
-		}
-
-		glog.Infof("Patch: %v", string(patchBytes))
+	patchBytes, err := json.Marshal(patch)
+	if err != nil {
+		glog.Error("Error marshalling patch!")
 		return &v1beta1.AdmissionResponse{
-			Allowed: true,
-			Patch:   patchBytes,
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
 		}
 	}
 
+	glog.Infof("Patch: %v", string(patchBytes))
 	return &v1beta1.AdmissionResponse{
 		Allowed: true,
+		Patch:   patchBytes,
 	}
 }
 
 // Serve method for webhook server
-func (whsvr *WebhookServer) serve(w http.ResponseWriter, r *http.Request) {
+func (whsvr *webhookServer) serve(w http.ResponseWriter, r *http.Request) {
 	var body []byte
 	if r.Body != nil {
 		if data, err := ioutil.ReadAll(r.Body); err == nil {
