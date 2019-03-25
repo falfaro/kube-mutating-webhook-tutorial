f78ad8e612fe22977137d9003e10bd64bb527df2
Felipe Alfaro Solana <felipe.alfaro@gmail.com>
2019-03-25 11:40:03 +0100

    Use proper names.

--- deployment/webhook-create-signed-cert.sh
+++ deployment/webhook-create-signed-cert.sh
@@ -4,10 +4,10 @@ set -e
 
 usage() {
     cat <<EOF
-Generate certificate suitable for use with an sidecar-injector webhook service.
+Generate certificate suitable for use with an ingress-injector webhook service.
 
 This script uses k8s' CertificateSigningRequest API to a generate a
-certificate signed by k8s CA suitable for use with sidecar-injector webhook
+certificate signed by k8s CA suitable for use with ingress-injector webhook
 services. This requires permissions to create and approve CSR. See
 https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster for
 detailed explantion and additional instructions.
@@ -46,9 +46,9 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-[ -z ${service} ] && service=sidecar-injector-webhook-svc
-[ -z ${secret} ] && secret=sidecar-injector-webhook-certs
-[ -z ${namespace} ] && namespace=default
+[ -z ${service} ] && service=ingress-webhook-svc
+[ -z ${secret} ] && secret=ingress-webhook-certs
+[ -z ${namespace} ] && namespace=kubeprod
 
 if [ ! -x "$(command -v openssl)" ]; then
     echo "openssl not found"
